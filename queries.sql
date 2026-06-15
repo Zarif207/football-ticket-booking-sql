b02639599d248f5bcef4beec24d6d571b08dc911
Zarif Hasan <zarifhasan207@gmail.com>
2026-06-15 18:47:03 +0600

    Add query to retrieve booking details with payment status

--- queries.sql
+++ queries.sql
@@ -11,3 +11,12 @@ FROM users
 WHERE full_name ILIKE 'Tanvir%'
 OR full_name ILIKE '%Haque%';
 
+-- query-3
+SELECT
+    booking_id,
+    user_id,
+    match_id,
+    COALESCE(payment_status, 'Action Required') AS systematic_status
+FROM bookings
+WHERE payment_status IS NULL;
+
